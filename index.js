13f952ecafd441fb20898bbd8f9c64fbe1911327
manuwar90 <drummer.manu@gmail.com>
2022-04-30 20:22:56 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-import * as d3 from 'd3';
+//Gráfico de columnas. Mi 1º gráfico
+var data = [
+  { year: 1991, name: 'alpha', value: 15 },
+  { year: 1992, name: 'alpha', value: 20 },
+];
+
+var visualization = d3plus
+  .viz()
+  .container('#viz1')
+  .data(data)
+  .type('bar')
+  .id('name')
+  .x('year')
+  .y('value')
+  .draw();
